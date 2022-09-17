e686d6c72257a45c1e7aeb635a4c7bc7d1b1fdd6
Irina <104638154+IrinaDG@users.noreply.github.com>
2022-09-17 18:01:12 +0200

    First commit

---
+++ feedback-app/src/App.js
@@ -0,0 +1,38 @@
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import {useState} from 'react'
+import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import {v4 as uuidv4} from 'uuid'
+
+function App(){
+
+  const [feedback, setFeedback] = useState(FeedbackData)
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete this comment?'))  
+    setFeedback(feedback.filter((item) => item.id !==id))
+  }
+
+  const addFeedback = (newFeedback) =>{
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback,...feedback])
+  }
+
+    
+  return(
+    <>
+    <Header />
+      <div className = "container">
+          <FeedbackForm handleAdd={addFeedback}/>
+          <FeedbackStats feedback ={feedback} />
+        <FeedbackList feedback={feedback} handleDelete= {deleteFeedback} /> 
+      </div>
+    </>
+    )
+
+}
+
+
+
+export default App
