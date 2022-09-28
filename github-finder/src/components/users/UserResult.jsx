6604a66d528266cd7141ad90d098c8435ed74a7b
Irina <104638154+IrinaDG@users.noreply.github.com>
2022-09-28 19:41:46 +0200

    Added loading spinner

--- github-finder/src/components/users/UserResult.jsx
+++ github-finder/src/components/users/UserResult.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useEffect, useState} from 'react'
+import Spinner from '../layout/Spinner'
 
 
 function UserResult() {
@@ -32,7 +33,7 @@ function UserResult() {
     </div>
   ) 
   }else{
-      return <h3>Loading...</h3>
+      return <Spinner />
   }
 
   
