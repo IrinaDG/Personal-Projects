88ac3df97377d82fa7238e68de89e3f84dc4e4f0
Irina <104638154+IrinaDG@users.noreply.github.com>
2022-09-29 17:38:40 +0200

    Creating search component with functionalities

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -14,33 +14,52 @@ export const GithubProvider = ({children}) => {
 
      const [state, dispatch] = useReducer(gitHubReducer, initialState)
 
-//Get initial users (for testing purposes)
+    //Get search results
 
-    const fetchUsers = async () => {
+    const searchUsers = async(text) => {
+        
         setLoading()
-        const response = await fetch(`${GITHUB_URL}/users`, {
+        
+        const params = new URLSearchParams({
+            q: text
+        })
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
         })
   
-        const data = await response.json()
+        const {items} = await response.json()
+        
         dispatch({
             type:'GET_USERS',
-            payload:data,
+            payload: items,
         })
-  //Loading state      
+        console.log(items);
+     }
+
+       //Clear users
+    
+        const clearUsers = () => dispatch({
+            type:'CLEAR_USERS'
+        })
+
+
+        
+        //Loading state 
+             
         const setLoading = () => dispatch({
-            type:'SET_LOADING'
-            
+            type:'SET_LOADING'  
         })
-        console.log(data);
-    }
+        
+        
+    
 
     return <GithubContext.Provider value = {{
         users: state.users,
         loading: state.loading,
-        fetchUsers
+        searchUsers,
+        clearUsers,
     }}>
         {children}
     </GithubContext.Provider>
