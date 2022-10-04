c6b8a20289dcbe65acc0256f913ecbefb9f4a72d
Irina <104638154+IrinaDG@users.noreply.github.com>
2022-10-04 17:21:23 +0200

    Displaying user details, inserting styles and fetching user repos details

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import { createRoutesFromChildren } from "react-router-dom";
 import gitHubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
@@ -7,8 +8,11 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({children}) => {
-     const initialState = {
+     
+    const initialState = {
          users: [],
+         user: {},
+         repos: [],
          loading: false
      }
 
@@ -28,13 +32,66 @@ export const GithubProvider = ({children}) => {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
         })
-        console.log(items);
+        
         
         const {items} = await response.json()
         
+
         dispatch({
             type:'GET_USERS',
             payload: items,
+        })
+        
+     }
+
+     //Get a user
+
+    const getUser = async(login) => {   
+        
+        setLoading()
+        
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        })
+
+        if(response.status ===404){
+            window.location = '/notfound'
+        }else {
+            const data = await response.json()
+            
+            dispatch({
+            type:'GET_USER',
+            payload: data,
+        })
+        
+        }
+            
+     }
+
+     //Get user repos
+
+    const getUserRepos = async(login) => {
+        setLoading()
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page: 10
+        })
+        
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+        
+        
+        const data = await response.json()
+        
+
+        dispatch({
+            type:'GET_REPOS',
+            payload: data,
         })
         
      }
@@ -58,9 +115,13 @@ export const GithubProvider = ({children}) => {
 
     return <GithubContext.Provider value = {{
         users: state.users,
+        user:state.user,
         loading: state.loading,
+        repos: state.repos,
         searchUsers,
         clearUsers,
+        getUser,
+        getUserRepos,
     }}>
         {children}
     </GithubContext.Provider>
