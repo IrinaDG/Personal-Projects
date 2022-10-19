3b3a6a1d7c0e07296ca3eda0a2213d3c20539db8
Irina <104638154+IrinaDG@users.noreply.github.com>
2022-10-19 13:57:10 +0200

    Added authentification to register a user and connect it to firebase

--- house-marketplace-app/src/pages/SignUp.jsx
+++ house-marketplace-app/src/pages/SignUp.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import {db} from '../firebase.config'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -23,6 +25,28 @@ function SignUp() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      
+      const user = userCredential.user
+      
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -31,7 +55,7 @@ function SignUp() {
             Welcome! Please enter your credentials to create an account!
           </p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="text"
             className="nameInput"
